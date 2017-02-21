4954bb9fd367444807c69f34c93cd5ece9f11184
James Chapman <james@paymenteconomics.com>
2017-02-21 12:42:50 -0500

    Reformatted code and added Royals

--- titanic_V2.R
+++ titanic_V2.R
@@ -1,6 +1,6 @@
 #This script loads the Kaggle Titanic dataset and conducts
 #a machine learning exercise on it and writes a Kaggle submission
-# 
+#
 # Author : James T.E. Chapman
 # Creation Date : 19 Feb 2017
 
@@ -11,7 +11,7 @@ library(caret)
 library(caretEnsemble)
 
 ## The usual
-rm(list=ls())
+rm(list = ls())
 
 # These are the constants that run the analysis
 
@@ -21,25 +21,58 @@ test.file  <- "./test.csv"
 base.submission.file <- "titanic_submission.csv"
 
 #A list of titles from training and test sets
-titles <-c( "Mr", "Mrs","Miss","Master",
-            "Don","Rev","Dr","Mme",
-            "Ms","Major","Lady",
-            "Sir","Mlle",
-            "Col","Capt",
-            "the","Jonkheer",
-            "Dona","RM","RF")
+titles <- c(
+  "Mr",
+  "Mrs",
+  "Miss",
+  "Master",
+  "Don",
+  "Rev",
+  "Dr",
+  "Mme",
+  "Ms",
+  "Major",
+  "Lady",
+  "Sir",
+  "Mlle",
+  "Col",
+  "Capt",
+  "the",
+  "Jonkheer",
+  "Dona",
+  "RM",
+  "RF"
+)
 
 # Rare titles and royal titles for consolidation
-rare.title.male <- c("Capt","Col","Don","Dr","Jonkheer",
-                     "Major","Rev","Sir","RF","RM")
-rare.title.female.miss <- c("Mme","Ms")
-rare.title.female.mrs <- c("Lady","Mlle","the","Dona")
-royal.titles <- titles[c(5,11,12,16,17,18)]
+rare.title.male <- c("Capt",
+                     "Col",
+                     "Don",
+                     "Dr",
+                     "Jonkheer",
+                     "Major",
+                     "Rev",
+                     "Sir",
+                     "RF",
+                     "RM")
+rare.title.female.miss <- c("Mme", "Ms")
+rare.title.female.mrs <- c("Lady", "Mlle", "the", "Dona")
+royal.titles <- titles[c(5, 11, 12, 16, 17, 18)]
 
 #Variables to focus on in the analysis
-analysis.vars <- c("test","Pclass","Age","Sex",
-                   "Parch","SibSp","Fare", 
-                   "Embarked", "title","Survived")
+analysis.vars <- c(
+  "test",
+  "Pclass",
+  "Age",
+  "Sex",
+  "Parch",
+  "SibSp",
+  "Fare",
+  "Embarked",
+  "title",
+  "Survived",
+  "royal"
+)
 
 
 # Now load and setup the data
@@ -53,11 +86,11 @@ rm(test.file)
 
 #consolidate both data sets to perform some substitutions
 train$test <- FALSE
-test$test <-TRUE
+test$test <- TRUE
 test$Survived <- -1
 
-full.data <- rbind(train,test)
-rm(train,test)
+full.data <- rbind(train, test)
+rm(train, test)
 
 #Now create the submission file
 pass.id <- full.data %>% filter(test) %>% select(PassengerId)
@@ -66,74 +99,78 @@ submission <- data.frame(PassengerId = pass.id)
 #data cleaning and preprocessing
 
 #split the name column
-tmp <- str_split(full.data$Name,",",simplify = TRUE)
+tmp <- str_split(full.data$Name, ",", simplify = TRUE)
 #extract last names
-full.data$lastName <- tmp[,1]
+full.data$lastName <- tmp[, 1]
 #extract titles
-full.data$title <- str_split(tmp[,2],boundary("word"),simplify = TRUE)[,1]
+full.data$title <-
+  str_split(tmp[, 2], boundary("word"), simplify = TRUE)[, 1]
 rm(tmp)
 #make a marker for royal (i.e. upper class) titles
 full.data <- full.data %>% mutate(royal = title %in% royal.titles)
 
 #clear up the rare titles and turn them into one of Miss,Mrs, Mr, Master
-full.data$title[full.data$title %in% rare.title.female.miss] <- "Miss"
+full.data$title[full.data$title %in% rare.title.female.miss] <-
+  "Miss"
 full.data$title[full.data$title %in% rare.title.female.mrs] <- "Mrs"
 full.data$title[full.data$title %in% rare.title.male] <- "Mr"
 
-cleaned.data <- full.data[,analysis.vars]
-cleaned.data <- cleaned.data %>% 
-        mutate(Pclass = factor(Pclass,ordered=TRUE)) %>% 
-        mutate(Sex = factor(Sex)) %>% 
-        mutate(Embarked = factor(Embarked)) %>% 
-        mutate(title = factor(title)) %>% 
-        mutate(family = SibSp + Parch) %>% 
-        mutate(family = cut(family,breaks=c(0,1,2,3,4,10),
-                            include.lowest = TRUE,right=FALSE))
- 
+cleaned.data <- full.data[, analysis.vars]
+cleaned.data <- cleaned.data %>%
+  mutate(Pclass = factor(Pclass, ordered = TRUE)) %>%
+  mutate(Sex = factor(Sex)) %>%
+  mutate(Embarked = factor(Embarked)) %>%
+  mutate(title = factor(title)) %>%
+  mutate(family = SibSp + Parch) %>%
+  mutate(family = cut(
+    family,
+    breaks = c(0, 1, 2, 3, 4, 10),
+    include.lowest = TRUE,
+    right = FALSE
+  ))
+
 #Repartition the data for analysis
-x.train <- cleaned.data %>% filter(!test) %>% select(-test) %>% 
+x.train <- cleaned.data %>% filter(!test) %>% select(-test) %>%
   mutate(Survived = factor(Survived))
 #Note need to fix this imputation to something better
 x.train$Embarked[is.na(x.train$Embarked)] <- "S"
-levels(x.train$Survived) <- c("dead","alive")
-x.test <- cleaned.data %>% filter(test) %>% select(-Survived,-test) 
+levels(x.train$Survived) <- c("dead", "alive")
+x.test <- cleaned.data %>% filter(test) %>% select(-Survived, -test)
 
 #Now do some preProcessing
-pre.x.train <- preProcess(x.train,method=c("bagImpute"))
-x.train <- predict(pre.x.train,x.train)
-pre.x.test <-  preProcess(x.test,method=c("bagImpute"))
-x.test <- predict(pre.x.test,x.test)
+pre.x.train <- preProcess(x.train, method = c("bagImpute"))
+x.train <- predict(pre.x.train, x.train)
+pre.x.test <-  preProcess(x.test, method = c("bagImpute"))
+x.test <- predict(pre.x.test, x.test)
 
 #Now do the analysis
-rf.model <- train(Survived~.,
-                  data=x.train,
-                  method="rf",
-                  trControl=trainControl(
-                    method = "LOOCV",
-                    savePredictions="final",
-                    classProbs=TRUE
-                  ))
+rf.model <- train(
+  Survived ~ .,
+  data = x.train,
+  method = "rf",
+  trControl = trainControl(
+    method = "LOOCV",
+    savePredictions = "final",
+    classProbs = TRUE
+  )
+)
 #make an RF submission file
-submission$Survived <- predict(rf.model,x.test)
-levels(submission$Survived) <- c("0","1")
+submission$Survived <- predict(rf.model, x.test)
+levels(submission$Survived) <- c("0", "1")
 #create a submission name
-submission.file <- paste0("rf",base.submission.file)
-write.csv(submission, file = submission.file, row.names=FALSE)
+submission.file <- paste0("rf", base.submission.file)
+write.csv(submission, file = submission.file, row.names = FALSE)
 
-gbm.model <- train(Survived~.,
-                   data=x.train,
-                   method="gbm",
-                   trControl=trainControl(
-                     savePredictions="final",
-                     classProbs=TRUE
-                   ))
+gbm.model <- train(
+  Survived ~ .,
+  data = x.train,
+  method = "gbm",
+  trControl = trainControl(savePredictions = "final",
+                           classProbs = TRUE)
+)
 #and a GBM submission file
-submission$Survived <- predict(gbm.model,x.test)
-levels(submission$Survived) <- c("0","1")
+submission$Survived <- predict(gbm.model, x.test)
+levels(submission$Survived) <- c("0", "1")
 #create a submission name
-submission.file <- paste0("gbm",base.submission.file)
-write.csv(submission, file = submission.file, row.names=FALSE)
-
-
-
-
+submission.file <- paste0("gbm", base.submission.file)
+write.csv(submission, file = submission.file, row.names = FALSE)
